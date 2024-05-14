f21202c83a36bb660e736a9f08468e577e10aebb
DaniilKorshkov <DaniilKorshkov@cmail.carleton.ca>
2024-05-14 17:26:57 +0000

    Add files via upload

---
+++ pages/SettingsPage.py
@@ -0,0 +1,4 @@
+import StreamlitGUI as sg
+import GUI_settings_menu as gsm  #page with settings to change default configuration of other pages
+
+gsm.Settings_Menu("GUI_settings","gui_settings_backup")
