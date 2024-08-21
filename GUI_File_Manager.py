57e7d55f788c1494610ca08ab5a3bc41cdf4e744
DaniilKorshkov <DaniilKorshkov@cmail.carleton.ca>
2024-08-21 19:13:45 +0000

    Add files via upload

---
+++ GUI_File_Manager.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import subprocess
+from JSONoperators import ReadJSONConfig
+import json
+
+
+
+
+def if_file_is_spectrum(filename):
+    try:
+        handle = open(f"{filename}","r")
+        for line in handle:
+            dictline = json.loads(line)
+            if dictline["class"] == "metadata" and dictline["is_a_spectrum"] == "True":
+                return True
+                break
+        handle.close()
+        return False
+
+    except:
+        return False
+
+
+
+def FindAllSpectrums(MainConfig="MainConfig"):
+    scans_folder = ReadJSONConfig("storage","scans_folder",MainConfig)
+
+    ret = str((subprocess.run(f"ls",capture_output=True)).stdout)
+
+    ret = ret.strip("b")
+    ret = ret.strip("'")
+
+    files_list = ret.split("\\n")
+    final_files_list = []
+
+    for file in files_list:
+        if if_file_is_spectrum(file):
+            final_files_list.append(file)
+
+    return final_files_list
+
+
+def GUI_File_Info(filename,MainConfig="MainConfig"):
+    handle = open(filename,"r")
+    for line in handle:
+        dictline = json.loads(line)
+        if dictline["class"] == "metadata":
+            valve = dictline["valve_number"]
+            init_scan = dictline["initial_value"]
+            amt_of_scans = dictline["amount_of_scans"]
+            step = dictline["step"]
+            return f"{str(filename)} parameters: \nValve:{valve}; Initial M:{init_scan}, amount of scans:{amt_of_scans}, step:{step}"
+
+
+def CreateSpectrum(filelist, MainConfig="MainConfig"):
+
+    first_line = {"class":"metadata","is_a_spectrum":"True"}
+
+    name = st.text_input(label="Type new spectrum name")
+    valve = st.text_input(label="Type multi inlet valve position")
+    init_m = st.text_input(label="Type initial M")
+    scans = st.text_input(label="Type amount of scans")
+    step = st.text_input(label="Type step")
+
+    create_new_spectrum = st.button("Create spectrum")
+    if create_new_spectrum:
+
+            valve = int(valve)
+            assert valve >=1 and valve <= 16
+
+            assert not(name in filelist)
+
+            init_m = float(init_m)
+            scans = int(scans)
+            step = float(step)
+
+            first_line["initial_value"] = init_m
+            first_line["amount_of_scans"] = scans
+            first_line["step"] = step
+
+            line = json.dumps(first_line)
+            subprocess.run(["touch",name])
+
+            handle = open(name,"w")
+            handle.write(line)
+            handle.close()
+
+
+
+
+
+
+def SpectrumFileManager(MainConfig="MainConfig"):
+    filelist = FindAllSpectrums(MainConfig)
+    st.write("Parameters of all spectrum files:")
+    st.markdown("")
+    for file in filelist:
+        try:
+            line = GUI_File_Info(file,MainConfig)
+            st.write(line)
+        except:
+            pass
+
+
+    for i in range(4):
+        st.markdown("")
+
+    st.write("Create new spectrum file:")
+    CreateSpectrum(filelist,MainConfig)
+
+
+
+
+
+
---
+++ VSC_Gui_Operation.py
@@ -0,0 +1,147 @@
+import StreamlitGUI as sg
+import streamlit as st
+import VSC_comms as vscc
+from JSONoperators import ReadJSONConfig
+import asyncio
+import servo_motor as sm
+
+def pressure_meter_gui(MainConfig="MainConfig"):
+    try:
+        pressure = vscc.ReadPressureGauge(MainConfig)
+        st.write(f"Pressure Meter pressure: {pressure} torr")
+    except:
+        pg_port = ReadJSONConfig("vsc", "pressure_gauge_port")
+        st.write(f"Failed to connect to pressure meter. Make sure it is connected to port {pg_port}")
+
+
+def pressure_controller_gui(MainConfig="MainConfig"):
+    try:
+        pressure = vscc.ReadPCPressure(MainConfig)
+        mode = vscc.ReadPCMode(MainConfig)
+        current_sp = vscc.ReadPCSetpoint(MainConfig)
+
+        st.write(f"Pressure Controller pressure: {pressure}")
+        st.write(f"Pressure Controller mode: {mode}")
+        st.write(f"Pressure Controller setpoint: {current_sp}")
+
+        col1,col2,col3 = st.columns(3)
+        with col1:
+            openpc = st.button("Open Pressure Controller")
+            if openpc:
+                vscc.ChangePCMode("Open",MainConfig)
+
+        with col2:
+            closepc = st.button("Close Pressure Controller")
+            if closepc:
+                vscc.ChangePCMode("Close Pressure Controller", MainConfig)
+
+        with col3:
+            setpointpc = st.button("Setpoint Mode")
+            if setpointpc:
+                vscc.ChangePCMode("Setpoint", MainConfig)
+
+
+        new_setpoint = st.text_input(label="Enter new PC setpoint")
+        change_sp = st.button("Apply setpoint")
+        try:
+            new_setpoint = float(new_setpoint)
+            if change_sp and new_setpoint >= 20 and new_setpoint <= 1000:
+                vscc.ChangePCPressure(new_setpoint,MainConfig)
+
+        except:
+            pass
+
+    except:
+        pc_port = ReadJSONConfig("vsc", "pressure_controller_port")
+        st.write(f"Failed to connect to pressure controller. . Make sure it is connected to port {pc_port}")
+
+
+
+
+
+def mfm_gui(MainConfig="MainConfig"):
+    try:
+        flow = vscc.ReadMFMFlowRate(MainConfig)
+        st.write(f"MFM flow rate: {flow} torr")
+    except:
+        mfm_port = ReadJSONConfig("vsc", "mfm_port")
+        st.write(f"Failed to connect to MFM. Make sure it is connected to port {mfm_port}")
+
+
+
+
+def mfc_gui(MainConfig="MainConfig"):
+    try:
+        flow = vscc.ReadMFCFlowRate(MainConfig)
+        mode = vscc.ReadMFCMode(MainConfig)
+        current_sp = vscc.ReadMFCSetpoint(MainConfig)
+
+        st.write(f"MFC flow rate: {flow}")
+        st.write(f"Pressure Controller mod: {mode}")
+        st.write(f"Pressure Controller setpoint: {current_sp}")
+
+        col1,col2,col3 = st.columns(3)
+        with col1:
+            openpc = st.button("Open MFC")
+            if openpc:
+                vscc.ChangeMFCMode("Open",MainConfig)
+
+        with col2:
+            closepc = st.button("Close MFC")
+            if closepc:
+                vscc.ChangeMFCMode("Close Pressure Controller", MainConfig)
+
+        with col3:
+            setpointpc = st.button("Setpoint Mode MFC")
+            if setpointpc:
+                vscc.ChangeMFCMode("Setpoint", MainConfig)
+
+
+        new_setpoint = st.text_input(label="Enter new MFC setpoint")
+        change_sp = st.button("Apply MFC setpoint")
+        try:
+            new_setpoint = float(new_setpoint)
+            if change_sp and new_setpoint >= 20 and new_setpoint <= 1000:
+                vscc.ChangePCPressure(new_setpoint,MainConfig)
+
+        except:
+            pass
+
+    except:
+        mfc_port = ReadJSONConfig("vsc", "mfc_port")
+        st.write(f"Failed to connect to MFC. . Make sure it is connected to port {mfc_port}")
+
+
+def vicivalve_gui(MainConfig="MainConfig"):
+    new_valve_position = st.text_input(label="Enter new multi inlet valve position")
+    apply_new_position = st.button("Apply new valve position")
+    if apply_new_position:
+        try:
+            new_valve_position = int(new_valve_position)
+            if new_valve_position >= 1 and new_valve_position <= 16:
+                sm.switch_valve_position(new_valve_position)
+            else:
+                st.write("Bad input")
+        except:
+            st.write("Bad input")
+
+
+
+
+
+def VSC_Gui(MainConfig="MainConfig"):
+    pressure_meter_gui(MainConfig)
+    for i in range(6):
+        st.markdown("")
+    pressure_controller_gui(MainConfig)
+    for i in range(6):
+        st.markdown("")
+    mfc_gui(MainConfig)
+    for i in range(6):
+        st.markdown("")
+    mfm_gui(MainConfig)
+    for i in range(6):
+        st.markdown("")
+    vicivalve_gui(MainConfig)
+
+
