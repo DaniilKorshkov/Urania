258b25a0ec9c9a76cad943585aaae1983c1c0a8b
DaniilKorshkov <DaniilKorshkov@cmail.carleton.ca>
2024-08-29 20:12:59 +0000

    Add files via upload

--- pages/Technical.py
+++ pages/Technical.py
@@ -1,6 +1,12 @@
 import RGA_comms as rga
 import streamlit as st
+import usb_autolocator
 
 changeip = st.button("Netdiscover for RGA IP")
+st.write("Please unplug VSC, oxygen analyzer and multi inlet valve from computer before allocating addresses")
+usb_discovery = st.button("Locate devices on USB bus")
+
 if changeip:
     rga.change_rga_ip("MainConfig")
+if usb_discovery:
+    usb_autolocator.allocate_usb_devices()
