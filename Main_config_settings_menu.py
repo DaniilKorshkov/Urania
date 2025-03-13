d7ac29447e8e80e1c981b1aea5328103b0448c80
Daniil <DaniilKorshkov@cmail.carleton.ca>
2025-03-13 12:11:32 -0400

    Removed unused code

--- Main_config_settings_menu.py
+++
@@ -1,9 +0,0 @@
-'''import JSONoperators as js
-import streamlit as st
-import StreamlitGUI as sg
-import json
-import datetime
-import GUI_settings_menu as gsm
-
-def ModifyNotifications():
-    Settings = js.read_main_config_settings("MainConfig","user_notifications")'''
