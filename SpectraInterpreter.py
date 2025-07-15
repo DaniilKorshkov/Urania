520540e724ece451c0ee767213e9849949617c29
Daniil <DaniilKorshkov@cmail.carleton.ca>
2025-07-15 08:51:34 -0400

    ionic current is divided over pressure

--- SpectraInterpreter.py
+++ SpectraInterpreter.py
@@ -1,11 +1,27 @@
 import numpy as np
 
 
-def solve_mass_spectrum(mass_spectrum, samples_dict):
+def solve_mass_spectrum(mass_spectrum, samples_dict, other_factors_dict):  # mass spectrum is interpreted as least-square solution of overdefined square equation
 
 
     for key in samples_dict:
         assert len(mass_spectrum) == len(samples_dict["key"])
+    
+    for key in other_factors_dict:
+        assert len(mass_spectrum) == len(other_factors_dict["key"])
+
+
+    
+    similar_entries_found = False
+    for key in samples_dict:
+        try:
+            void = other_factors_dict["key"]
+            similar_entries_found = True
+        except:
+            pass
+    
+    if similar_entries_found:
+        raise Exception("Two equivalent variables names were encountered in samples list and noise factors list")
 
 
 
@@ -19,6 +35,10 @@ def solve_mass_spectrum(mass_spectrum, samples_dict):
     for key in samples_dict:
         variables_list_raw.append(key)
         left_side_matrix_raw.append(samples_dict[key])
+    
+    for key in other_factors_dict:
+        variables_list_raw.append(key)
+        left_side_matrix_raw.append(other_factors_dict[key])
 
     
     left_side_matrix = np.array(left_side_matrix_raw)
@@ -28,18 +48,22 @@ def solve_mass_spectrum(mass_spectrum, samples_dict):
     
 
     solutions_numpy_array = np.linalg.lstsq(left_side_matrix, mass_spectrum)
-    solutions_dictionary = {}
+    samples_solutions_dictionary = {}
+    other_factors_solutions_dictionary = {}
 
     counter = 0
     for key in samples_dict:
         solutions_dictionary[key] = float(solutions_numpy_array[0][counter])
+        counter += 1
+    for key in other_factors_dict:
+        other_factors_solutions_dictionary[key] = float(solutions_numpy_array[0][counter])
         counter += 1
 
     
 
     stdev = float((solutions_numpy_array[1])[0])
 
-    return solutions_dictionary, stdev
+    return solutions_dictionary, other_factors_solutions_dictionary, stdev
 
 
 
