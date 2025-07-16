381144e740ef65140abe8b89ebfd756417ca9f68
Daniil <DaniilKorshkov@cmail.carleton.ca>
2025-07-16 12:06:59 -0400

    Fixed identation for Arduino GUI and added mock cracking patterns

--- VSC_Gui_Operation.py
+++ VSC_Gui_Operation.py
@@ -205,61 +205,61 @@ def ArduinoGUI():
             st.write(f"PT-06 pressure: {ret[5]} psi")
 
 
-            for i in range(6):
+        for i in range(6):
+            st.markdown("")
+
+
+        col1, col2 = st.columns(2)
+
+
+
+        with col1:
+
+            if ret[6] == "1":
+                st.write(f"Actuator 1 is open")
+            else:
+                st.write(f"Actuator 1 is closed")
+
+            for i in range(2):
                 st.markdown("")
 
 
-            col1, col2 = st.columns(2)
+            
+            turn_on_act_one = st.button("Open actuator 1")
+            turn_off_act_one = st.button("Close actuator 1")
+
+            if turn_on_act_one:
+                ArduinoComms.TurnActuatorOneOn()
+                st.write(f"Actuator 1 opened!")
+            if turn_off_act_one:
+                ArduinoComms.TurnActuatorOneOff()
+                st.write(f"Actuator 1 closed!")
 
 
 
-            with col1:
-
-                if ret[6] == "1":
-                    st.write(f"Actuator 1 is open")
-                else:
-                    st.write(f"Actuator 1 is closed")
-
-                for i in range(2):
-                    st.markdown("")
-
-
-                
-                turn_on_act_one = st.button("Open actuator 1")
-                turn_off_act_one = st.button("Close actuator 1")
-
-                if turn_on_act_one:
-                    ArduinoComms.TurnActuatorOneOn()
-                    st.write(f"Actuator 1 opened!")
-                if turn_off_act_one:
-                    ArduinoComms.TurnActuatorOneOff()
-                    st.write(f"Actuator 1 closed!")
+        with col2:
 
 
 
-            with col2:
+            if ret[8] == "1":
+                st.write(f"Actuator 2 is open")
+            else:
+                st.write(f"Actuator 2 is closed")
+
+            for i in range(2):
+                st.markdown("")
 
 
+            
+            turn_on_act_two = st.button("Open actuator 2")
+            turn_off_act_two = st.button("Close actuator 2")
 
-                if ret[8] == "1":
-                    st.write(f"Actuator 2 is open")
-                else:
-                    st.write(f"Actuator 2 is closed")
-
-                for i in range(2):
-                    st.markdown("")
-
-
-                
-                turn_on_act_two = st.button("Open actuator 2")
-                turn_off_act_two = st.button("Close actuator 2")
-
-                if turn_on_act_two:
-                    ArduinoComms.TurnActuatorTwoOn()
-                    st.write(f"Actuator 2 opened!")
-                if turn_off_act_two:
-                    ArduinoComms.TurnActuatorTwoOff()
-                    st.write(f"Actuator 2 closed!")
+            if turn_on_act_two:
+                ArduinoComms.TurnActuatorTwoOn()
+                st.write(f"Actuator 2 opened!")
+            if turn_off_act_two:
+                ArduinoComms.TurnActuatorTwoOff()
+                st.write(f"Actuator 2 closed!")
 
 
 
