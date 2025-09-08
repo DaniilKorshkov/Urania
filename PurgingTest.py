23d88f96c179f2ab89a5d1ee0e696b359e39fa78
daniil <DaniilKorshkov@cmail.carleton.ca>
2025-09-08 11:31:27 -0400

    Added scan amount customization

--- PurgingTest.py
+++ PurgingTest.py
@@ -7,7 +7,7 @@ import ArduinoComms
 
 
 
-def MakeMultiplePurges( purge_cycles_list=[3,4,5] , time_open_list=[25,30,35,40,45] , time_closed_list = [30,35,40,45,50] , pure_line = 14, contaminant_line = 8, calmdown_time = 30 ):
+def MakeMultiplePurges( purge_cycles_list=[3,4,5] , time_open_list=[25,30,35,40,45] , time_closed_list = [30,35,40,45,50] , pure_line = 14, contaminant_line = 8, calmdown_time = 30, scan_amount = 3 ):
     taskname = GetTask()
     spectrum_filename,amount_of_scans, valve_position, accuracy, purge_cycles_placeholder = GetTaskData(taskname)
 
@@ -39,8 +39,8 @@ def MakeMultiplePurges( purge_cycles_list=[3,4,5] , time_open_list=[25,30,35,40,
 
                 time.sleep(calmdown_time)
 
-
-                spectrum_to_analyze, intital_mass, step, ErrorMessage = RGA_comms.AppendSpectrumJSON(filename, accuracy=accuracy, custom_line_name = f"PC: {purge_cycles}, TO: {time_open}, TC: {time_closed}")
+                for i in range(scan_amount):
+                    spectrum_to_analyze, intital_mass, step, ErrorMessage = RGA_comms.AppendSpectrumJSON(filename, accuracy=accuracy, custom_line_name = f"PC: {purge_cycles}, TO: {time_open}, TC: {time_closed}")
 
 
 
