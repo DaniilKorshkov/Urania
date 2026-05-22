a5cb1578aca47b4e42bd0a72429b34baf5293f03
daniil <daniilkorshkov@cmail.carleton.ca>
2026-05-22 15:15:16 -0400

    Monte Carlo tested and working

--- SubprocessMonteCarlo.py
+++ SubprocessMonteCarlo.py
@@ -1,10 +1,9 @@
 import subprocess
 from scipy.special import erfinv
 from statistics import stdev
+from statistics import mean
 from random import random
-
-
-
+from sys import stdout
 
 
 def single_round():
@@ -20,7 +19,7 @@ def modify_arguments(arguments,arguments_stdev):
 
     for i in range(len(arguments)):
         if (arguments_stdev[i] == 0) or (arguments_stdev[i] == None) or (type(arguments_stdev[i]) != int and type(arguments_stdev[i]) != float) or (type(arguments[i]) != int and type(arguments[i]) != float ):
-            modified_arguments.append( arguments[i] )
+            modified_arguments.append( str (arguments[i]) )
         else:
             while True:
                 randomnumber = 2*random() - 1 
@@ -33,7 +32,7 @@ def modify_arguments(arguments,arguments_stdev):
             
             
 
-            modified_arguments.append( arguments[i] + stdev_quantity*arguments_stdev[i] )
+            modified_arguments.append( str( arguments[i] + 1.41421356237*stdev_quantity*arguments_stdev[i] ) )
 
 
     assert ( len(modified_arguments) == len(arguments) )
@@ -43,44 +42,79 @@ def modify_arguments(arguments,arguments_stdev):
 
 def main(command,arguments,arguments_stdev,cwd,amount_of_rounds=50000):
 
-    assert( len[arguments] == len[arguments_stdev] )
+    assert( len(arguments) == len(arguments_stdev) )
 
     output_matrix = []
 
+    percents_finished = 0
+    finished_counter_step = 100 / amount_of_rounds
+
+
+
+    modified_arguments = modify_arguments(arguments,[0]*len(arguments))
+    arguments_list = [command] + modified_arguments
+    most_probable_output =  (((subprocess.run(arguments_list,capture_output=True,cwd=cwd)).stdout).decode("utf-8")).strip("\n")
+    most_probable_output_array = most_probable_output.split(" ")
+
+
+
+
+
     for i in range(amount_of_rounds):
 
-        modified_arguments = modify_arguments(arguments,arguments_stdev):
+        modified_arguments = modify_arguments(arguments,arguments_stdev)
 
+       
+        
+        arguments_list = [command] + modified_arguments
         
 
-        single_execution_output = subprocess.run([( [command].append(modified_arguments) ),comma-separated-arguements],capture_output=True,cwd=cwd)
-
+        single_execution_output = (((subprocess.run(arguments_list,capture_output=True,cwd=cwd)).stdout).decode("utf-8")).strip("\n")
+        
+        
         single_execution_output_array = single_execution_output.split(" ")
+
+        
         
         output_matrix.append(single_execution_output_array)
 
 
+        percents_finished += finished_counter_step
+        
+        #print(f"{percents_finished} % finished ",end="\r")
 
-    
+        stdout.flush()
+        stdout.write(f"\r{percents_finished} % finished ")
+
+
+
+    output_stdev_array = []
+    mean_minus_mp_array = []
     for i in range(len(output_matrix[0])):
 
-        output_stdev_array = []
+        
 
         try:
-            placeholder =  float(output_matrix[j])[i]
+            
+            placeholder =  float ((output_matrix[0])[i])
+            
         except:       
             output_stdev_array.append(0)
             continue
 
         single_output_dataset = []
         for j in range( amount_of_rounds ):
-            single_output_dataset.append( ( float(output_matrix[j])[i]) )
+            single_output_dataset.append( ( float ((output_matrix[j])[i])) )
+        
             
         output_stdev_array.append( stdev(single_output_dataset) )
+        mean_minus_mp_array.append( mean(single_output_dataset) - float(most_probable_output_array[i]) )
     
 
-    return output_stdev_array
+    return output_stdev_array, mean_minus_mp_array
 
 
 if __name__ == "__main__":
-    main()
+    array_to_print, mean_array = main("/home/daniil/Urania/sum_computer",[5,5],[1,1],"/home/daniil/Urania",amount_of_rounds=25000)
+    print(array_to_print)
+    print(mean_array)
--- root_test.C
+++ root_test.C
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <cstdlib> // For std::atoi
-#include "TH1F.h"
-#include "TFile.h"
-#include "TRandom3.h"
-#include "TCanvas.h"
+
 
 
 int main(int argc, char* argv[]) {
@@ -14,12 +11,13 @@ int main(int argc, char* argv[]) {
     }
 
     // Convert the arguments from strings to integers
-    int num1 = std::atoi(argv[1]);
-    int num2 = std::atoi(argv[2]);
+    float num1 = std::atof(argv[1]);
+    float num2 = std::atof(argv[2]);
 
     // Calculate and print the sum
-    int sum = num1 + num2;
-    std::cout << "Sum: " << sum << std::endl;
+    float sum = num1 + num2;
+    float product = num1*num2;
+    std::cout << sum << " " << product << std::endl;
 
 
 
@@ -27,13 +25,13 @@ int main(int argc, char* argv[]) {
 
 
     // Create a 1D histogram: name, title, bins, x_min, x_max
-    TH1F *hist = new TH1F("hist", "Basic Histogram;X axis;Counts", 100, -5, 5);
+            ///TH1F *hist = new TH1F("hist", "Basic Histogram;X axis;Counts", 100, -5, 5);
 
     // Fill with random Gaussian data
-    TRandom3 rng(42);
-    for (int i = 0; i < 10000; i++) {
-        hist->Fill(rng.Gaus(0, 1));
-    }
+    ///TRandom3 rng(42);
+    ///for (int i = 0; i < 10000; i++) {
+    ///    hist->Fill(rng.Gaus(0, 1));
+    ///}
 
 
 
@@ -42,12 +40,12 @@ int main(int argc, char* argv[]) {
 
 
         // Save to a ROOT file
-    TFile *file = new TFile("histogram.root", "RECREATE");
-    hist->Write();
-    file->Close();
+    ///TFile *file = new TFile("histogram.root", "RECREATE");
+    ///hist->Write();
+    ///file->Close();
 
-    delete hist;
-    delete file;
+    ///delete hist;
+    ///delete file;
 
 
 
